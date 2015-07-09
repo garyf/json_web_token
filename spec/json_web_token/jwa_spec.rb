07f3f4428d90dda7c9b530729aa83ad558791f83
Gary Fleshman <gfleshman@newforge-tech.com>
2015-07-08 20:53:53 -0400

    spec Jwa, Jws refactor

--- spec/json_web_token/jwa_spec.rb
+++ spec/json_web_token/jwa_spec.rb
@@ -2,47 +2,49 @@ require 'json_web_token/jwa'
 
 module JsonWebToken
   describe Jwa do
-    context 'detect changed signature or data' do
-      let(:valid_data) { 'valid.data' }
-      let(:other_data) { 'other.data' }
-
+    context 'detect changed signing_input or MAC' do
+      let(:signing_input) { 'signing_input' }
+      let(:changed_signing_input) { 'changed_signing_input' }
       shared_examples_for '#signed' do
-        it 'and #verified?' do
-          signature = Jwa.signed(algorithm, private_key, valid_data)
-          a_signature = Jwa.signed(algorithm, private_key, other_data)
+        it 'is #verified?' do
+          mac = Jwa.signed(algorithm, signing_key, signing_input)
+          expect(Jwa.verified? mac, algorithm, verifying_key, signing_input).to be true
+          expect(Jwa.verified? mac, algorithm, verifying_key, changed_signing_input).to be false
 
-          expect(Jwa.verified? signature, algorithm, private_key, valid_data).to be true
-          expect(Jwa.verified? signature, algorithm, private_key, other_data).to be false
-          expect(Jwa.verified? a_signature, algorithm, private_key, valid_data).to be false
-        end
-      end
-
-      describe 'w/o key' do
-        let(:algorithm) { 'HS256' }
-        it 'raises' do
-          expect { Jwa.signed(algorithm, nil, valid_data) }
-            .to raise_error(RuntimeError, 'Invalid key')
-        end
-      end
-
-      shared_examples_for 'unrecognized' do
-        it 'raises' do
-          expect { Jwa.signed(algorithm, private_key, valid_data) }
-            .to raise_error(RuntimeError, 'Unrecognized algorithm')
+          changed_mac = Jwa.signed(algorithm, signing_key, changed_signing_input)
+          expect(Jwa.verified? changed_mac, algorithm, verifying_key, signing_input).to be false
         end
       end
 
       describe 'HS256' do
-        let(:private_key) { 'this_a_32_character_private_key!' }
         let(:algorithm) { 'HS256' }
+        let(:signing_key) { 'gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C' }
+        let(:verifying_key) { signing_key }
         it_behaves_like '#signed'
       end
+    end
 
-      describe 'invalid algorithm' do
-        let(:private_key) { 'secret' }
-        ['HT256', 'HS257', '', nil].each do |elt|
-          let(:algorithm) { "#{elt}" }
-          it_behaves_like 'unrecognized'
+    context 'param validation' do
+      let(:data) { 'data' }
+      context 'w HS256 key' do
+        let(:key) { 'gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C' }
+        describe 'unrecognized algorithm' do
+          ['HT256', 'HS257', '', nil].each do |elt|
+            let(:algorithm) { "#{elt}" }
+            it 'raises' do
+              expect { Jwa.signed(algorithm, key, data) }
+                .to raise_error(RuntimeError, 'Unrecognized algorithm')
+            end
+          end
+        end
+
+        describe 'HS256' do
+          let(:algorithm) { 'HS256' }
+          it 'returns a 32-byte MAC string' do
+            mac = Jwa.signed(algorithm, key, data)
+            expect(mac.bytesize).to eql 32
+            expect(mac.class).to eql String
+          end
         end
       end
     end
--- spec/json_web_token/jws_spec.rb
+++ spec/json_web_token/jws_spec.rb
@@ -2,74 +2,92 @@ require 'json_web_token/jws'
 
 module JsonWebToken
   describe Jws do
-    context '#message_signature' do
-      let(:key) { 'this_a_32_character_private_key!' }
+    context 'w payload' do
       let(:payload) { 'payload' }
-
-      shared_examples_for 'w #validate' do
-        it 'verified' do
-          serialized_output = Jws.message_signature(header, payload, key)
-          expect(Jws.validate serialized_output, algorithm, key).to eql serialized_output
-        end
-      end
-
-      context 'w HS256' do
-        let(:algorithm) { 'HS256' }
-
-        context "w matching 'alg' header parameter" do
-          let(:header) { {alg: 'HS256'} }
-          it_behaves_like 'w #validate'
-
-          describe 'w/o passing key to #validate' do
-            it 'invalid' do
-              serialized_output = Jws.message_signature(header, payload, key)
-              expect(Jws.validate serialized_output, algorithm, nil).to eql 'Invalid'
-            end
+      context '#message_signature' do
+        shared_examples_for 'w #validate' do
+          it 'verified' do
+            jws = Jws.message_signature(header, payload, signing_key)
+            expect(Jws.validate jws, algorithm, verifying_key).to eql jws
           end
         end
 
-        describe "w/o an implemented 'alg' header parameter" do
-          let(:header) { {alg: 'other'} }
+        context 'w HS256 keys' do
+          let(:signing_key) { 'gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C' }
+          let(:verifying_key) { signing_key }
+          context "w HS256 'alg' header parameter" do
+            let(:header) { {alg: 'HS256'} }
+            describe 'w passing a matching algorithm to #validate' do
+              let(:algorithm) { 'HS256' }
+              it_behaves_like 'w #validate'
+
+              describe 'w/o passing key to #validate' do
+                it "returns 'Invalid'" do
+                  jws = Jws.message_signature(header, payload, signing_key)
+                  expect(Jws.validate jws, algorithm, nil).to eql 'Invalid'
+                end
+              end
+            end
+
+            describe 'w/o passing a matching algorithm to #validate' do
+              let(:algorithm) { 'RS256' }
+              it 'raises' do
+                jws = Jws.message_signature(header, payload, signing_key)
+                expect { Jws.validate(jws, algorithm, verifying_key) }
+                  .to raise_error(RuntimeError, "Algorithm not matching 'alg' header parameter")
+              end
+            end
+          end
+        end
+      end
+
+      context 'header validation' do
+        let(:signing_key) { 'signing_key' }
+        describe "w/o a recognized 'alg' header parameter" do
+          let(:header) { {alg: 'HS257'} }
           it 'raises' do
-            expect { Jws.message_signature(header, payload, key) }
+            expect { Jws.message_signature(header, payload, signing_key) }
               .to raise_error(RuntimeError, 'Unrecognized algorithm')
           end
         end
 
-        describe "w/o required 'alg' header parameter" do
+        describe "w/o a required 'alg' header parameter" do
           let(:header) { {typ: 'JWT'} }
           it 'raises' do
-            expect { Jws.message_signature(header, payload, key) }
+            expect { Jws.message_signature(header, payload, signing_key) }
               .to raise_error(RuntimeError, "Missing required 'alg' header parameter")
           end
         end
+      end
 
-        describe '#validate w/o a matching algorithm' do
-          let(:header) { {alg: 'HS256'} }
-          it 'raises' do
-            serialized_output = Jws.message_signature(header, payload, key)
-            expect { Jws.validate(serialized_output, 'none', key) }
-              .to raise_error(RuntimeError, "Algorithm not matching 'alg' header parameter")
+      context '#unsecured_jws' do
+        context 'w valid header' do
+          let(:header) { {alg: 'none'} }
+          describe 'w passing a matching algorithm to #validate' do
+            let(:algorithm) { 'none' }
+            it 'is verified' do
+              jws = Jws.unsecured_jws(header, payload)
+              expect(Jws.validate jws, algorithm).to eql jws
+            end
+          end
+
+          describe 'w/o passing a matching algorithm to #validate' do
+            let(:algorithm) { 'HS256' }
+            let(:verifying_key) { 'gZH75aKtMN3Yj0iPS4hcgUuTwjAzZr9C' }
+            it 'raises' do
+              jws = Jws.unsecured_jws(header, payload)
+              expect { Jws.validate(jws, algorithm, verifying_key) }
+                .to raise_error(RuntimeError, "Algorithm not matching 'alg' header parameter")
+            end
           end
         end
-      end
-    end
 
-    context '#unsecured_jws' do
-      let(:payload) { 'payload' }
-      describe 'w matching algorithm' do
-        let(:header) { {alg: 'none'} }
-        it 'verified' do
-          serialized_output = Jws.unsecured_jws(header, payload)
-          expect(Jws.validate serialized_output, 'none').to eql serialized_output
-        end
-      end
-
-      describe 'w/o matching algorithm' do
-        let(:header) { {alg: 'HS256'} }
-        it 'raises' do
-          expect { Jws.unsecured_jws(header, payload) }
-            .to raise_error(RuntimeError, "Invalid 'alg' header parameter")
+        describe 'w invalid header' do
+          let(:header) { {alg: 'HS256'} }
+          it 'raises' do
+            expect { Jws.unsecured_jws(header, payload) }
+              .to raise_error(RuntimeError, "Invalid 'alg' header parameter")
+          end
         end
       end
     end
